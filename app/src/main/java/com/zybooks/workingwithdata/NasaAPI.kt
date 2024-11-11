4c69f66dbed1ca2b563008dd41355414c4cdc53a
Harley Davis <asphixiatedaphid@gmail.com>
2024-11-11 15:38:30 -0800

    Added clear button and code to clear edit text fields. Added onclick listeners to search and clear buttons.

--- app/src/main/java/com/zybooks/workingwithdata/NasaAPI.kt
+++ app/src/main/java/com/zybooks/workingwithdata/NasaAPI.kt
@@ -2,6 +2,7 @@ package com.zybooks.workingwithdata
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
@@ -67,6 +68,16 @@ class NasaAPI : AppCompatActivity() {
 
             }
         }
+
+        var searchButton:Button = findViewById(R.id.searchButton)
+        searchButton.setOnClickListener {
+            searchAPOD()
+        }
+        var clearButton: Button = findViewById(R.id.clearButton)
+        clearButton.setOnClickListener {
+            clearEditTextFields()
+        }
+
         imageDataSet = arrayListOf(Pair("https://apod.nasa.gov/apod/image/1908/EtnaCloudsMoon_Giannobile_960.jpg", "What's happening above that volcano? Although Mount Etna is seen erupting, the clouds are not related to the eruption. They are lenticular clouds formed when moist air is forced upwards near a mountain or volcano.  The surreal scene was captured by chance late last month when the astrophotographer went to Mount Etna, a UNESCO World Heritage Site in Sicily, Italy, to photograph the conjunction between the Moon and the star Aldebaran. The Moon appears in a bright crescent phase, illuminating an edge of the lower lenticular cloud.  Red hot lava flows on the right.  Besides some breathtaking stills, a companion time-lapse video of the scene shows the lenticular clouds forming and wavering as stars trail far in the distance.    Follow APOD in English on: Instagram, Facebook,  Reddit, or Twitter"),
             Pair("https://img.youtube.com/vi/f8rs3bcEO-o/0.jpg", "What would it look like to orbit a black hole? Many black holes are surrounded by swirling pools of gas known as accretion disks. These disks can be extremely hot, and much of the orbiting gas will eventually fall through the black hole's event horizon -- where it will never be seen again. The featured animation is an artist's rendering of the curious disk spiraling around the supermassive black hole at the center of spiral galaxy NGC 3147.  Gas at the inner edge of this disk is so close to the black hole that it moves unusually fast -- at 10 percent of the speed of light. Gas this fast shows relativistic beaming, making the side of the disk heading toward us appear significantly brighter than the side moving away.  The animation is based on images of NGC 3147 made recently with the Hubble Space Telescope.    Astrophysicists: Browse 2,000+ codes in the Astrophysics Source Code Library"),
             Pair("https://apod.nasa.gov/apod/image/1908/HumanSpaceship2_TsevisHubbleRJN_960.jpg", "You are a spaceship soaring through the universe. So is your dog. We all carry with us trillions of microorganisms as we go through life. These multitudes of bacteria, fungi, and archaea have different DNA than you. Collectively called your microbiome, your shipmates outnumber your own cells. Your crew members form communities, help digest food, engage in battles against intruders, and sometimes commute on a liquid superhighway from one end of your body to the other.  Much of what your microbiome does, however, remains unknown. You are the captain, but being nice to your crew may allow you to explore more of your local cosmos."),
@@ -80,5 +91,15 @@ class NasaAPI : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 2)
         recyclerView.adapter = imageCustomAdapter
 
+    }
+
+    private fun searchAPOD() {
+        TODO("Not yet implemented")
+    }
+
+    private fun clearEditTextFields() {
+        countEditText.text.clear()
+        startDateEditText.text.clear()
+        endDateEditText.text.clear()
     }
 }
